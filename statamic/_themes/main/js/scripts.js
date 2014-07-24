eb2605f957f7afe341d6e302a4fdb6b4c1363949
Philippe Gauthier <philippe@philippe.io>
2014-07-23 18:39:36 -0700

    removed about layout

--- statamic/_themes/main/js/scripts.js
+++ statamic/_themes/main/js/scripts.js
@@ -7981,5 +7981,19 @@ $('.upgrades-scroll').click(function () {
 
 $('#upgrades').css({'min-height':(($(window).height())-256)+'px'});
 
+$(document).ready(function(){
+// Instafeed
+var feed = new Instafeed({
+    get: 'user',
+    clientId: 'f61fb2669e734e2da2587b457f0afe57',
+    accessToken: '501656935.f61fb26.c99de54c939c432bb20f5dd282ef4e33',
+    userId: 501656935,
+    resolution: 'standard_resolution',
+    template: '<div class="instagram-wrapper"><div class="flipcard"><a target=_blank href="{{link}}"><div class="front"><img src="{{image}}" /></div><div class="back"><p>{{caption}}</p></div></a></div></div>',
+    limit: 15
+      });
+feed.run();
+});
+
 
 
